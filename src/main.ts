531fd99bd124777879b0a9647e9c434d49bfa6a5
Tamta <tamtakopadz16@gmail.com>
2025-02-01 18:14:41 +0400

    refactoring

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
 
-  // CORS configuration - allow requests from specific domains
   app.enableCors({
-    // origin: [
-    //   'http://localhost:4200', // Local Angular app
-    //   'https://animal-farm-456c4.web.app', // Deployed Angular app
-    // ],
+    origin: [
+      'http://localhost:4200', // Local Angular app
+      'https://animal-farm-456c4.web.app', // Deployed Angular app
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
