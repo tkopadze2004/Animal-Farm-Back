ca925aa016c4bef6bff55bcc760d3dce4db7d3f0
Tamta <tamtakopadz16@gmail.com>
2025-02-01 19:53:53 +0400

    Set up MongoDB Atlas connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,11 @@ import { pigModule } from './animals/pig-status.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost:27017/animal-farm'),
+    // MongooseModule.forRoot('mongodb://localhost:27017/animal-farm'),
+    MongooseModule.forRoot(
+      'mongodb+srv://owner:smthSDnHa@cluster0.moyze.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=Cluster0',
+    ),
+
     AnimalsModule,
     pigModule,
   ],
