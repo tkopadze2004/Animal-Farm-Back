b5cb3c46e5e2a7b52933a0c46946156beac2a3e3
Tamta <tamtakopadz16@gmail.com>
2025-02-01 19:03:00 +0400

    update

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
+
   app.enableCors({
     origin: [
-      'http://localhost:4200', // Local Angular app
-      'https://animal-farm-456c4.web.app', // Deployed Angular app
+      'http://localhost:4200',
+      'https://animal-farm-456c4.web.app',
+      'https://animal-farm-456c4.firebaseapp.com',
     ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: 'GET,POST,PUT,DELETE',
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
     credentials: true,
   });
   await app.listen(process.env.PORT || 3000);
 }
-
 void bootstrap();
