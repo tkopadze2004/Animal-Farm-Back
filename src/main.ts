1e60beb38f73ee50956b0757cd212866af343fa9
Tamta <tamtakopadz16@gmail.com>
2025-02-01 17:50:25 +0400

    update

--- src/main.ts
+++ src/main.ts
@@ -4,20 +4,21 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
-
+  // CORS configuration - allow requests from localhost and other necessary domains
   app.enableCors({
-    origin: [
-      'http://localhost:4200',
-      'https://animal-farm-456c4.web.app',
-      'https://animal-farm-456c4.firebaseapp.com',
-    ],
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
+    // origin: [
+    //   'http://localhost:4200',
+    //   'https://animal-farm-456c4.web.app',
+    // ],
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
-  const PORT = process.env.PORT || 3000;
 
-  await app.listen(PORT, '0.0.0.0');
+  const PORT = process.env.PORT || 3000; // Use the port from environment or 3000 by default
+  await app.listen(PORT, '0.0.0.0'); // Listen on all interfaces
+  console.log(`Server running on port ${PORT}`);
 }
+
 void bootstrap();
